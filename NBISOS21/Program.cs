f245d4d52d3f44fdd8b3872ae11bc0ea6008fea2
IdasJohansson <idasusannajohansson@gmail.com>
2021-10-20 10:52:20 +0200

    test

---
+++ NBISOS21/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Vecka1 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in tre tal:  ");
+            int tal1 = Convert.ToInt32(Console.ReadLine());
+            int tal2 = Convert.ToInt32(Console.ReadLine());
+            int tal3 = Convert.ToInt32(Console.ReadLine());
+
+            int sum = tal1 + tal2 + tal3;
+
+            Console.WriteLine(sum);
+
+            if (tal1 > tal2 && tal1 > tal3)
+            {
+                Console.WriteLine(tal1);
+            }
+            if (tal2 > tal1 && tal2 > tal3)
+            {
+                Console.WriteLine(tal2);
+            }
+            else
+            {
+                Console.WriteLine(tal3);
+            }
+        }
+    }
+}
